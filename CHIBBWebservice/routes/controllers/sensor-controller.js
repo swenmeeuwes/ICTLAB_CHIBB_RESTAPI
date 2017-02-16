5d437f4ba14406eafc5ac0eac2d9b3a19f3959b4
swenmeeuwes <swenmeeu@gmail.com>
2017-02-16 23:57:18 +0100

    Added: Getter in sensor-controller

--- CHIBBWebservice/routes/controllers/sensor-controller.js
+++ CHIBBWebservice/routes/controllers/sensor-controller.js
@@ -11,26 +11,30 @@ var neo4j = require('neo4j-driver').v1;
 var driver = require('./../../database/driver');
 var session = driver.session();
 
-
+// To-do: Add wrapper
 router.get('/:id', function (req, res) {
-    res.send("Sensor data from " + req.params.id);
+    console.log("[SensorController] GET HTTP request received from %s", req.ip);
+
+    var result = session.run("MATCH (s:Sensor) WHERE s.id = {id} RETURN s AS Sensor", {id: req.params.id});
+    result.then(function (result) {
+        var records = result.records;
+        var recordFieldObjects = records.map(function (item) {
+            return item._fields[0].properties; // Extract fields from the record
+        });
+        res.send(recordFieldObjects);
+    }, function (errorMessage, errorCode) {
+        res.send({errorCode: errorCode, errorMessage: errorMessage});
+    });
 });
 
 router.post('/', function (req, res) {
-    console.log("Post request received from %s", req.id);
-
+    console.log("[SensorController] POST HTTP request received from %s", req.ip);
 
     session
-            .run("CREATE (a:Sensor {id: {id}, type: {type}})", {id: req.body.id, type: req.body.type})
+            .run("CREATE (s:Sensor {id: {id}, type: {type}})", req.body)
             .then(function () {
-
-//                session.close();
-//                driver.close();
-
                 res.send("POSTED SENSOR!\r\n" + JSON.stringify(req.body));
             });
-
-
 });
 
 module.exports = router;

68c4ea9763ddde3d1f13747a49358e0b70f6e605
swenmeeuwes <swenmeeu@gmail.com>
2017-02-18 00:10:50 +0100

    Added: HTTP PUT + HTTP DELETE, completing the CRUD

--- CHIBBWebservice/routes/controllers/sensor-controller.js
+++ CHIBBWebservice/routes/controllers/sensor-controller.js
@@ -11,11 +11,24 @@ var neo4j = require('neo4j-driver').v1;
 var driver = require('./../../database/driver');
 var session = driver.session();
 
+// CRUD
+// CREATE
+router.post('/', function (req, res) {
+    console.log("[SensorController] POST HTTP request received from %s", req.ip);
+
+    session
+            .run("CREATE (s:Sensor {id: {id}, type: {type}});", req.body)
+            .then(function () {
+                res.send("POSTED SENSOR!\r\n" + JSON.stringify(req.body));
+            });
+});
+
+// READ
 // To-do: Add wrapper
 router.get('/:id', function (req, res) {
     console.log("[SensorController] GET HTTP request received from %s", req.ip);
 
-    var result = session.run("MATCH (s:Sensor) WHERE s.id = {id} RETURN s AS Sensor", {id: req.params.id});
+    var result = session.run("MATCH (s:Sensor) WHERE s.id = {id} RETURN s AS Sensor;", {id: req.params.id});
     result.then(function (result) {
         var records = result.records;
         var recordFieldObjects = records.map(function (item) {
@@ -27,13 +40,31 @@ router.get('/:id', function (req, res) {
     });
 });
 
-router.post('/', function (req, res) {
-    console.log("[SensorController] POST HTTP request received from %s", req.ip);
+// UPDATE
+router.put('/:id', function (req, res) {
+    console.log("[SensorController] PUT HTTP request received from %s", req.ip);
+
+    var queryParams = req.body;
+    queryParams.id = req.params.id;
+
+    var updatedSensors = session.run("MATCH (s:Sensor) WHERE s.id = {id} SET s.type = {type} RETURN s AS Sensor;", queryParams);
+    updatedSensors.then(function(result) {
+        if(result.records.length > 0)
+            res.send("PUT SENSOR!\r\n" + JSON.stringify(result));
+        res.send("COULDN'T PUT SENSOR, SENSOR WITH ID DOESN'T EXSIST");
+    }, function (errorMessage, errorCode) {
+        res.send({errorCode: errorCode, errorMessage: errorMessage});
+    });
+});
+
+// DELETE
+router.delete('/:id', function (req, res) {
+    console.log("[SensorController] DELETE HTTP request received from %s", req.ip);
 
     session
-            .run("CREATE (s:Sensor {id: {id}, type: {type}})", req.body)
+            .run("MATCH (s:Sensor) WHERE s.id = {id} DETACH DELETE s;", {id: req.params.id})
             .then(function () {
-                res.send("POSTED SENSOR!\r\n" + JSON.stringify(req.body));
+                res.send("DELETED SENSOR with id " + req.params.id);
             });
 });
 

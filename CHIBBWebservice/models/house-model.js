ec0468b56b295ca542c190bca3f3cab2f650cdeb
jvanbreda <jvbreda1995@gmail.com>
2017-03-29 11:30:36 +0200

    Added CRUD operations for the houses of the database

--- CHIBBWebservice/models/house-model.js
+++ CHIBBWebservice/models/house-model.js
@@ -8,17 +8,139 @@
 
 var houseModel = {};
 
-houseModel.getAll = function (session) {
-//    return new Promise((resolve, reject) => {
-//        
-//    });
+var House = function (properties) {
+    this.hid = properties.hid;
+    this.address = properties.address;
+};
 
+houseModel.constructor = House;
+
+// Get all houses in the database (for admin purposes)
+houseModel.getAllHouses = function (session) {
+    return new Promise(function (resolve, reject) {
+        var houses = session.run("MATCH (h:House) return h AS House;");
+        houses.then(function (result) {
+            if (result.records[0]) {
+                var houseArray = [];
+                for (var i = 0; i < result.records.length; i++) {
+                    houseArray.push(new House(result.records[i]._fields[0].properties));
+                }
+                resolve(houseArray);
+            }
+            else {
+                resolve([]);
+            }
+        });
+    });
     session.close();
 };
 
-houseModel.getByID = function (session, id) {
-    // return promise
-    
+// Get all houses owned by the logged in user
+houseModel.getUserHouses = function (session, username) {
+    return new Promise(function (resolve, reject) {
+        var houses = session.run("MATCH (u:User {username:{username}})-[:Owns]->(h:House) RETURN h AS House;", {username: username});
+        houses.then(function (result) {
+            if (result.records[0]) {
+                var houseArray = [];
+                for (var i = 0; i < result.records.length; i++) {
+                    houseArray.push(new House(result.records[i]._fields[0].properties));
+                }
+                resolve(houseArray);
+            }
+            else {
+                resolve([]);
+            }
+        });
+    });
+    session.close();
+};
+
+houseModel.getById = function (session, username, hid) {
+    return new Promise(function (resolve, reject) {
+        var houses = session.run("MATCH (u:User {username:{username}})-[:Owns]->(h:House {hid: {hid}}) RETURN h AS House;", {username: username, hid: hid});
+        houses.then(function (result) {
+            if (result.records[0]) {
+                var houseArray = [];
+                for (var i = 0; i < result.records.length; i++) {
+                    houseArray.push(new House(result.records[i]._fields[0].properties));
+                }
+                resolve(houseArray);
+            }
+            else {
+                resolve([], {message: "House does not exist or is not yours!"});
+            }
+        });
+    });
+    session.close();
+};
+
+houseModel.createHouse = function (session, username, requestBody) {
+    return new Promise(function (resolve, reject) {
+        var house = session.run("MATCH (h:House {hid:{hid}}) return h AS House;", {hid: requestBody.hid});
+        house.then(function (result) {
+            if (result.records[0]) {
+                reject({message: "House with that Id already exists!"});
+            }
+            else {
+                var newHouse = session.run("MATCH (u:User {username:{username}}) CREATE ((u) -[r:Owns]-> (h:House{hid:{hid},address:{address}}));", {username: username, hid: requestBody.hid, address: requestBody.address});
+                newHouse.then(function () {
+                    resolve(new House(requestBody));
+                });
+            }
+        });
+    });
+    session.close();
+};
+
+houseModel.updateHouse = function (session, username, hid, requestBody) {
+    return new Promise(function (resolve, reject) {
+        var house = session.run("MATCH (h:House {hid:{hid}}) return h AS House;", {hid: hid});
+        house.then(function (result) {
+            if (result.records[0]) {
+                var house = session.run("MATCH (u:User {username:{username}}) -[r:Owns]-> (h:House{hid:{hid}}) RETURN h AS House;", {username: username, hid: hid});
+                house.then(function (result) {
+                    if (result.records[0]) {
+                        var updatedHouse = session.run("MATCH (h:House {hid:{hid}}) SET h += {hid: {newId}, address: {newAddress}};", {hid: hid, newId: requestBody.hid, newAddress: requestBody.address});
+                        updatedHouse.then(function () {
+                            resolve(new House(requestBody));
+                        });
+                    }
+                    else {
+                        reject({message: "House with that id is not yours!"});
+                    }
+                });
+            }
+            else {
+                reject({message: "House with that id does not exist!"});
+            }
+        });
+    });
+    session.close();
+};
+
+houseModel.deleteHouse = function (session, username, hid) {
+    return new Promise(function (resolve, reject) {
+        var house = session.run("MATCH (h:House {hid:{hid}}) return h AS House;", {hid: hid});
+        house.then(function (result) {
+            if (result.records[0]) {
+                var house = session.run("MATCH (u:User {username:{username}}) -[r:Owns]-> (h:House{hid:{hid}}) RETURN h AS House;", {username: username, hid: hid});
+                house.then(function (result) {
+                    if (result.records[0]) {
+                        var updatedHouse = session.run("MATCH (h:House {hid:{hid}}) DETACH DELETE h", {hid: hid});
+                        updatedHouse.then(function () {
+                            resolve(new House({hid: hid}));
+                        });
+                    }
+                    else {
+                        reject({message: "House with that id is not yours!"});
+                    }
+                });
+            }
+            else {
+                reject({message: "House with that id does not exist!"});
+            }
+        });
+    });
     session.close();
 };
 
--- CHIBBWebservice/routes/house.js
+++ CHIBBWebservice/routes/house.js
@@ -11,14 +11,75 @@ var router = express.Router();
 
 var dbConnector = require('../database/database-connector');
 
+var houseModel = require('../models/house-model');
+
 // middleware that is specific to this router
 //router.use(function timeLog (req, res, next) {
 //  console.log('Time: ', Date.now())
 //  next()
 //});
 
-router.get('/', function(req, res) {
-    res.ok(["yo"]);
+router.get('/getall', function(req, res) {
+    var getPromise = houseModel.getAllHouses(dbConnector.getSession(req));
+    getPromise.then(function (data) {
+        if (data.length > 0) {
+            res.ok(data);
+        }
+        else {
+            res.nocontent(data);
+        }
+    });
+});
+
+router.get('/', function(req, res){
+    var getPromise = houseModel.getUserHouses(dbConnector.getSession(req), res.locals.username);
+    getPromise.then(function(data){
+        if (data.length > 0) {
+            res.ok(data);
+        }
+        else {
+            res.nocontent(data);
+        }
+    });
+});
+
+router.get('/:id', function(req, res){
+    var getPromise = houseModel.getById(dbConnector.getSession(req), res.locals.username, req.params.id);
+    getPromise.then(function(data){
+        if (data.length > 0) {
+            res.ok(data);
+        }
+        else {
+            res.nocontent(data);
+        }
+    });
+});
+
+router.post('/', function(req, res){
+    var createPromise = houseModel.createHouse(dbConnector.getSession(req), res.locals.username, req.body);
+    createPromise.then(function(data){
+        res.created({house: data});
+    }).catch(function(error){
+        res.ok(error.message);
+    });
+});
+
+router.put('/:id', function(req, res){
+    var updatePromise = houseModel.updateHouse(dbConnector.getSession(req), res.locals.username, req.params.id, req.body);
+    updatePromise.then(function(data){
+        res.ok({updatedHouse: data});
+    }).catch(function(error){
+        res.ok(error.message);
+    });
+});
+
+router.delete('/:id', function(req, res){
+    var deletePromise = houseModel.deleteHouse(dbConnector.getSession(req), res.locals.username, req.params.id);
+    deletePromise.then(function(data){
+        res.ok({deletedHouse: data});
+    }).catch(function(error){
+        res.ok(error.message);
+    });
 });
 
 module.exports = router;

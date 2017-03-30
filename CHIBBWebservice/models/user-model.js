7fa5fece41f2dfe76909679184796ddd4b2e5745
jvanbreda <jvbreda1995@gmail.com>
2017-03-30 16:32:41 +0200

    processed feedback from code review (error handling middleware will be implemented later)

--- CHIBBWebservice/models/user-model.js
+++ CHIBBWebservice/models/user-model.js
@@ -8,7 +8,7 @@
 
 var SHA256 = require('crypto-js/sha256');
 
-var userModel = {}; // To-do: Capitalize
+var UserModel = {};
 
 var User = function (properties) {
     this.username = properties.username;
@@ -18,9 +18,9 @@ var User = function (properties) {
     this.secret = properties.secret;
 };
 
-userModel.constructor = User;
+UserModel.constructor = User;
 
-userModel.login = function (session, requestBody) {
+UserModel.login = function (session, requestBody) {
     return new Promise(function (resolve, reject) {
         var user = session.run("MATCH (u:User {username:{username}}) return u AS User;", {username: requestBody.username});
         user.then(function (result) {
@@ -28,35 +28,39 @@ userModel.login = function (session, requestBody) {
                 var properties = result.records[0]._fields[0].properties;
                 var hashedPassword = properties.password;
                 if (SHA256(requestBody.password + properties.salt).toString() === hashedPassword) {
+                    session.close();
                     resolve();
                 } else {
+                    session.close();
                     reject({error: 403, message: "Wrong password"});
                 }
             } else {
+                session.close();
                 reject({error: 404, message: "No such user found!"});
             }
         });
     });
 };
 
-userModel.register = function (session, requestBody) {
+UserModel.register = function (session, requestBody) {
     return new Promise(function (resolve, reject) {
         var user = session.run("MATCH (u:User {username:{username}}) RETURN u AS User;", {username: requestBody.username});
         user.then(function (result) {
             if (result.records[0]) {
+                session.close();
                 reject({message: "Username already in use!"});
             } else {
                 var newUser = session.run("CREATE (u:User {username:{username},password:{password}, email: {email}, salt: {salt}, secret: {secret}});", requestBody);
                 newUser.then(function () {
+                    session.close();
                     resolve(new User(requestBody));
                 });
             }
         });
     });
-    session.close();
 };
 
-userModel.getAll = function (session) {
+UserModel.getAll = function (session) {
     return new Promise(function (resolve, reject) {
         var users = session.run("MATCH (u:User) return u AS User;");
         users.then(function (result) {
@@ -65,20 +69,20 @@ userModel.getAll = function (session) {
                 for (var i = 0; i < result.records.length; i++) {
                     userArray.push(new User(result.records[i]._fields[0].properties)); // Find a method to do this properly (result.records ... ._fields
                 }
+                session.close();
                 resolve(userArray);
             } else {
+                session.close();
                 resolve([]);
             }
         });
     });
-
-    session.close();
 };
 
-userModel.getByID = function (session, id) {
+UserModel.getByID = function (session, id) {
     // return promise
 
     session.close();
 };
 
-module.exports = userModel;
+module.exports = UserModel;

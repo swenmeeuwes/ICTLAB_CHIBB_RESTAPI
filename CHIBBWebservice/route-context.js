47a641f780263d415ea5f76fff40a8ed30f9c05f
Swen Meeuwes <swenmeeu@gmail.com>
2017-06-01 16:56:57 +0200

    Made an '/api' route which leads to the apidoc

--- CHIBBWebservice/route-context.js
+++ CHIBBWebservice/route-context.js
@@ -6,10 +6,15 @@
  * A context for defining routes within the API
  **/
 
+var express = require('express');
+var path = require('path');
 var isAuthenticated = require('./middlewares/is-authenticated');
 var routes = require('./routes');
 
-exports.configure = function(router) {   
+exports.configure = function(router) {
+    // ApiDocs
+    router.use('/api', express.static(path.join(__dirname, '/apidoc')));
+    
     // Add routes here
     router.use('/house', isAuthenticated, routes.house);
     router.use('/sensor', isAuthenticated, routes.sensor);
